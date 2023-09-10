e4f2d4f522a636217324492cab61e84cf1130c28
joris xu <joris xu@DESKTOP-DGODQ2L>
2023-09-10 03:01:35 +0200

    test

--- src/startup/Main.java
+++ src/startup/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	//start the whole cycle
 
-	//test with laptop eclipse
+	//test with laptop eclipse 2
 	public static void main(String[] args) {
 		new WebScraperApplicatie(new DomeinController()).start();
 	}
